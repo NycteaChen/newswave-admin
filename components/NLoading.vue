326033ec8a0fcda18acd93f93d7d6ffdeedb72d0
NycteaChen <jungturn01tw@gmail.com>
2024-06-27 18:34:57 +0800

    feat(nTable): 加上 table 元件
    調整:
    
    1. 從前台複製 table 等相關元件
    
    2. 加上跟 page 相關的型別

---
+++ components/NLoading.vue
@@ -0,0 +1,42 @@
+<template>
+  <section
+    class="n-loading-container position-relative"
+    :class="{ show: loading }"
+  >
+    <slot />
+    <div v-show="loading">
+      <div
+        class="n-loading text-primary position-absolute top-0 flex-column gap-3 start-0 w-100 h-100 d-flex justify-content-center align-items-center"
+      >
+        <n-spin :is-small="isSmall" />
+        <span v-if="showText">讀取中...</span>
+      </div>
+    </div>
+  </section>
+</template>
+<script lang="ts" setup>
+import type { NSpinProps } from '@/components/NSpin.vue';
+
+interface NLoadingProps {
+  loading: boolean;
+  showText?: boolean;
+  isSmall?: NSpinProps['isSmall'];
+}
+
+withDefaults(defineProps<NLoadingProps>(), {
+  loading: false,
+  showText: true,
+  isSmall: false
+});
+</script>
+<style lang="scss" scoped>
+.n-loading-container {
+  &.show {
+    min-height: 300px;
+  }
+}
+
+.n-loading {
+  background: rgba($gray-100, 0.8);
+}
+</style>
